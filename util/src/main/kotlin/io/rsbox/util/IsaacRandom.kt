cc10c8cc95d3c581fa2a292bfc787062d8b77459
kyle-escobar <kslrtips@gmail.com>
2019-08-30 19:32:23 -0400

    Reworked IsaacRandom for codebeat score.

--- util/src/main/kotlin/io/rsbox/util/IsaacRandom.kt
+++ util/src/main/kotlin/io/rsbox/util/IsaacRandom.kt
@@ -1,13 +1,6 @@
 package io.rsbox.util
 
 /**
- *
- *
- * An implementation of the [ISAAC](http://www.burtleburtle.net/bob/rand/isaacafa.html) psuedorandom number
- * generator.
- *
- *
- * <pre>
  * ------------------------------------------------------------------------------
  * Rand.java: By Bob Jenkins.  My random number generator, ISAAC.
  * rand.init() -- initialize
@@ -17,75 +10,108 @@ package io.rsbox.util
  * 970719: use context, not global variables, for internal state
  * 980224: Translate to Java
  * ------------------------------------------------------------------------------
-</pre> *
- *
- *
- * This class has been changed to be more conformant to Java and javadoc conventions.
- *
- *
- * @author Bob Jenkins
  */
-class IsaacRandom {
 
-    /**
-     * The accumulator.
-     */
-    private var a: Int = 0
+class IsaacRandom/* equivalent to randinit(ctx, TRUE) after putting seed in randctx in C */(seed: IntArray) {
+    var count: Int = 0                           /* count through the results in rsl[] */
+    var rsl: IntArray                                /* the results given to the user */
+    private var mem: IntArray? = null                                   /* the internal state */
+    private var a: Int = 0                                              /* accumulator */
+    private var b: Int = 0                                          /* the last result */
+    private var c: Int = 0              /* counter, guarantees cycle is at least 2^^40 */
 
-    /**
-     * The last result.
-     */
-    private var b: Int = 0
 
-    /**
-     * The counter.
-     */
-    private var c: Int = 0
-
-    /**
-     * The count through the results in the results array.
-     */
-    private var count: Int = 0
-
-    /**
-     * The internal state.
-     */
-    private var mem: IntArray
-
-    /**
-     * The results given to the user.
-     */
-    private var rsl: IntArray
-
-    /**
-     * Creates the random number generator without an initial seed.
-     */
-    constructor() {
-        mem = IntArray(SIZE)
-        rsl = IntArray(SIZE)
-        init(false)
-    }
-
-    /**
-     * Creates the random number generator with the specified seed.
-     *
-     * @param seed The seed.
-     */
-    constructor(seed: IntArray) {
+    init {
         mem = IntArray(SIZE)
         rsl = IntArray(SIZE)
         for (i in seed.indices) {
             rsl[i] = seed[i]
         }
-        init(true)
+        Init(true)
     }
 
-    /**
-     * Initialises this random number generator.
-     *
-     * @param hasSeed Set to `true` if a seed was passed to the constructor.
-     */
-    private fun init(hasSeed: Boolean) {
+
+    /* Generate 256 results.  This is a fast (not small) implementation. */
+    fun Isaac() {
+        var i = 0
+        var j: Int = SIZE / 2
+        var x: Int
+        var y: Int
+
+        b += ++c
+        while (i < SIZE / 2) {
+            x = mem!![i]
+            a = a xor (a shl 13)
+            a += mem!![j++]
+            y = mem!![x and MASK shr 2] + a + b
+            mem!![i] = y
+            b = mem!![y shr SIZEL and MASK shr 2] + x
+            rsl[i++] = b
+
+            x = mem!![i]
+            a = a xor a.ushr(6)
+            a += mem!![j++]
+            y = mem!![x and MASK shr 2] + a + b
+            mem!![i] = y
+            b = mem!![y shr SIZEL and MASK shr 2] + x
+            rsl[i++] = b
+
+            x = mem!![i]
+            a = a xor (a shl 2)
+            a += mem!![j++]
+            y = mem!![x and MASK shr 2] + a + b
+            mem!![i] = y
+            b = mem!![y shr SIZEL and MASK shr 2] + x
+            rsl[i++] = b
+
+            x = mem!![i]
+            a = a xor a.ushr(16)
+            a += mem!![j++]
+            y = mem!![x and MASK shr 2] + a + b
+            mem!![i] = y
+            b = mem!![y shr SIZEL and MASK shr 2] + x
+            rsl[i++] = b
+        }
+
+        j = 0
+        while (j < SIZE / 2) {
+            x = mem!![i]
+            a = a xor (a shl 13)
+            a += mem!![j++]
+            y = mem!![x and MASK shr 2] + a + b
+            mem!![i] = y
+            b = mem!![y shr SIZEL and MASK shr 2] + x
+            rsl[i++] = b
+
+            x = mem!![i]
+            a = a xor a.ushr(6)
+            a += mem!![j++]
+            y = mem!![x and MASK shr 2] + a + b
+            mem!![i] = y
+            b = mem!![y shr SIZEL and MASK shr 2] + x
+            rsl[i++] = b
+
+            x = mem!![i]
+            a = a xor (a shl 2)
+            a += mem!![j++]
+            y = mem!![x and MASK shr 2] + a + b
+            mem!![i] = y
+            b = mem!![y shr SIZEL and MASK shr 2] + x
+            rsl[i++] = b
+
+            x = mem!![i]
+            a = a xor a.ushr(16)
+            a += mem!![j++]
+            y = mem!![x and MASK shr 2] + a + b
+            mem!![i] = y
+            b = mem!![y shr SIZEL and MASK shr 2] + x
+            rsl[i++] = b
+        }
+    }
+
+
+    /* initialize, or reinitialize, this instance of rand */
+    fun Init(flag: Boolean) {
         var i: Int
         var a: Int
         var b: Int
@@ -95,14 +121,14 @@ class IsaacRandom {
         var f: Int
         var g: Int
         var h: Int
-        h = GOLDEN_RATIO
+        h = -0x61c88647
         g = h
         f = g
         e = f
         d = e
         c = d
         b = c
-        a = b
+        a = b                        /* the golden ratio */
 
         i = 0
         while (i < 4) {
@@ -134,8 +160,8 @@ class IsaacRandom {
         }
 
         i = 0
-        while (i < SIZE) { /* fill in mem[] with messy stuff */
-            if (hasSeed) {
+        while (i < SIZE) {              /* fill in mem[] with messy stuff */
+            if (flag) {
                 a += rsl[i]
                 b += rsl[i + 1]
                 c += rsl[i + 2]
@@ -169,28 +195,28 @@ class IsaacRandom {
             h = h xor a.ushr(9)
             c += h
             a += b
-            mem[i] = a
-            mem[i + 1] = b
-            mem[i + 2] = c
-            mem[i + 3] = d
-            mem[i + 4] = e
-            mem[i + 5] = f
-            mem[i + 6] = g
-            mem[i + 7] = h
+            mem?.set(i, a)
+            mem?.set(i + 1, b)
+            mem?.set(i + 2, c)
+            mem?.set(i + 3, d)
+            mem?.set(i + 4, e)
+            mem?.set(i + 5, f)
+            mem?.set(i + 6, g)
+            mem?.set(i + 7, h)
             i += 8
         }
 
-        if (hasSeed) { /* second pass makes all of seed affect all of mem */
+        if (flag) {           /* second pass makes all of seed affect all of mem */
             i = 0
             while (i < SIZE) {
-                a += mem[i]
-                b += mem[i + 1]
-                c += mem[i + 2]
-                d += mem[i + 3]
-                e += mem[i + 4]
-                f += mem[i + 5]
-                g += mem[i + 6]
-                h += mem[i + 7]
+                a += mem!![i]
+                b += mem!![i + 1]
+                c += mem!![i + 2]
+                d += mem!![i + 3]
+                e += mem!![i + 4]
+                f += mem!![i + 5]
+                g += mem!![i + 6]
+                h += mem!![i + 7]
                 a = a xor (b shl 11)
                 d += a
                 b += c
@@ -215,138 +241,25 @@ class IsaacRandom {
                 h = h xor a.ushr(9)
                 c += h
                 a += b
-                mem[i] = a
-                mem[i + 1] = b
-                mem[i + 2] = c
-                mem[i + 3] = d
-                mem[i + 4] = e
-                mem[i + 5] = f
-                mem[i + 6] = g
-                mem[i + 7] = h
+                mem!![i] = a
+                mem!![i + 1] = b
+                mem!![i + 2] = c
+                mem!![i + 3] = d
+                mem!![i + 4] = e
+                mem!![i + 5] = f
+                mem!![i + 6] = g
+                mem!![i + 7] = h
                 i += 8
             }
         }
 
-        isaac()
+        Isaac()
         count = SIZE
     }
 
-    /**
-     * Generates 256 results.
-     */
-    private fun isaac() {
-        var i: Int
-        var j: Int
-        var x: Int
-        var y: Int
-
-        b += ++c
-        i = 0
-        j = SIZE / 2
-        while (i < SIZE / 2) {
-            x = mem[i]
-            a = a xor (a shl 13)
-            a += mem[j++]
-            y = mem[x and MASK shr 2] + a + b
-            mem[i] = y
-            b = mem[y shr LOG_SIZE and MASK shr 2] + x
-            rsl[i++] = b
-
-            x = mem[i]
-            a = a xor a.ushr(6)
-            a += mem[j++]
-            y = mem[x and MASK shr 2] + a + b
-            mem[i] = y
-            b = mem[y shr LOG_SIZE and MASK shr 2] + x
-            rsl[i++] = b
-
-            x = mem[i]
-            a = a xor (a shl 2)
-            a += mem[j++]
-            y = mem[x and MASK shr 2] + a + b
-            mem[i] = y
-            b = mem[y shr LOG_SIZE and MASK shr 2] + x
-            rsl[i++] = b
-
-            x = mem[i]
-            a = a xor a.ushr(16)
-            a += mem[j++]
-            y = mem[x and MASK shr 2] + a + b
-            mem[i] = y
-            b = mem[y shr LOG_SIZE and MASK shr 2] + x
-            rsl[i++] = b
-        }
-
-        j = 0
-        while (j < SIZE / 2) {
-            x = mem[i]
-            a = a xor (a shl 13)
-            a += mem[j++]
-            y = mem[x and MASK shr 2] + a + b
-            mem[i] = y
-            b = mem[y shr LOG_SIZE and MASK shr 2] + x
-            rsl[i++] = b
-
-            x = mem[i]
-            a = a xor a.ushr(6)
-            a += mem[j++]
-            y = mem[x and MASK shr 2] + a + b
-            mem[i] = y
-            b = mem[y shr LOG_SIZE and MASK shr 2] + x
-            rsl[i++] = b
-
-            x = mem[i]
-            a = a xor (a shl 2)
-            a += mem[j++]
-            y = mem[x and MASK shr 2] + a + b
-            mem[i] = y
-            b = mem[y shr LOG_SIZE and MASK shr 2] + x
-            rsl[i++] = b
-
-            x = mem[i]
-            a = a xor a.ushr(16)
-            a += mem[j++]
-            y = mem[x and MASK shr 2] + a + b
-            mem[i] = y
-            b = mem[y shr LOG_SIZE and MASK shr 2] + x
-            rsl[i++] = b
-        }
-    }
-
-    /**
-     * Gets the next random value.
-     *
-     * @return The next random value.
-     */
-    fun nextInt(): Int {
-        if (0 == count--) {
-            isaac()
-            count = SIZE - 1
-        }
-        return rsl[count]
-    }
-
     companion object {
-
-        /**
-         * The golden ratio.
-         */
-        private val GOLDEN_RATIO = -0x61c88647
-
-        /**
-         * The log of the size of the result and memory arrays.
-         */
-        private val LOG_SIZE = 8
-
-        /**
-         * The size of the result and memory arrays.
-         */
-        private val SIZE = 1 shl LOG_SIZE
-
-        /**
-         * A mask for pseudorandom lookup.
-         */
-        private val MASK = SIZE - 1 shl 2
+        const val SIZEL = 8              /* log of size of rsl[] and mem[] */
+        const val SIZE = 1 shl SIZEL               /* size of rsl[] and mem[] */
+        const val MASK = SIZE - 1 shl 2            /* for pseudorandom lookup */
     }
-
 }
